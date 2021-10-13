b26b5ec007b73130e41b8301afddf8a4bd62abc8
Sanket <sanketbhosale.codekul@gmail.com>
2021-10-13 17:04:47 +0530

    Javascript intro

---
+++ javascript/js-intro.js
@@ -0,0 +1,34 @@
+// alert("Welcome to JS")
+
+
+// 3 keywords for declaring a variable in js
+
+// var , let, const
+
+// var keyword variables are global scope variable as well as function scope variable
+// let keyword variables are block scope variable as well as function scope variable
+//const keyword variable for a constant value variable
+var num1 = 20;
+
+
+console.log(num1)
+
+if(true){
+    console.log(num1)
+    
+    var num2 = 100;
+
+    console.log(num2)
+
+}
+
+
+function displayNum() {
+    var num3 = 150;
+    console.log(num3)
+}
+
+displayNum()
+console.log(num2)
+console.log(num3)
+
