e17311dd7e7265cbbbcb7f3bcd30eebbc0e46ddb
Sanket <sanketbhosale.codekul@gmail.com>
2021-10-18 17:00:57 +0530

    javascript operators

--- javascript/js-intro.js
+++ javascript/js-intro.js
@@ -21,29 +21,46 @@ let num1;
 num1=258
 console.log(num1)
 
-
+var fname="codekul"
 
 num1=35;
 
 
+num5=55;
+console.log(num5)
 
+let num5;
+
+
+
+//console.log(city)
+console.log(fname)
 
 if(true){
-    console.log(num1)
     
+    console.log(num1)
+    console.log(city)
     var num2 = 100;
 
     console.log(num2)
+
+    console.log(fname)
 
 }
 
 
 function displayNum() {
+    // var fname="codekul"
+
+    let city = "Pune"
     var num3 = 150;
     console.log(num3)
 }
 
 displayNum()
 console.log(num2)
-console.log(num3)
+//console.log(num3)
+
+console.log(fname)
+
 
