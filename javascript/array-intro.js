d5a02122fef7537540085486afca0ea1478cf94f
Sanket <sanketbhosale.codekul@gmail.com>
2021-10-26 16:59:24 +0530

    Array methods

--- javascript/array-intro.js
+++ javascript/array-intro.js
@@ -1,62 +1,87 @@
+let numbers = [
+  1,
+  "two",
+  { name: "codekul", tech: "MERN" },
+  4,
+  5,
+  ["a", "b", "c"],
+];
 
-        
-      let numbers = [
-        1,
-        "two",
-        { name: "codekul", tech: "MERN" },
-        4,
-        5,
-        ["a", "b", "c"],
-      ];
+let content = [
+  "codekul",
+  10,
+  20,
+  ["one", "two", "three"],
+  { name: "xyz", city: "pune" },
+];
 
-      let content = [
-        "codekul",
-        10,
-        20,
-        ["one", "two", "three"],
-        { name: "xyz", city: "pune" },
-      ];
+console.log(numbers);
 
-      console.log(numbers);
+console.log(content);
 
-      console.log(content);
+let blocks = new Array(100);
+console.log(blocks);
 
-      let blocks = new Array(100);
-      console.log(blocks);
+let nums = [10, 20, 25, 42];
 
-      let nums = [10, 20, 25, 42];
+console.log(nums);
 
-      console.log(nums);
+nums.map((i) => {
+  i *= 2;
 
-      nums.map((i) => {
-        i *= 2;
+  console.log(i);
+});
 
-        console.log(i);
-      });
+let newArray = numbers.concat(content);
+
+console.log(newArray);
+
+console.log(newArray.find((element) => element < 20));
+
+console.log(newArray.indexOf(20));
+
+console.log(newArray.length);
+
+let box = new Array(500);
+
+console.log(box);
+var x = 0;
+for (i = 0; i < 20; i++) {
+  x += i;
+
+  console.log(x);
+}
+
+let numarray = [10, 20, 30, 40, 60];
+
+let state = (previous, current) => {
+  console.log("State Value", previous * current);
+  return previous + current;
+};
 
 
-      let newArray = numbers.concat(content)
+console.log(numarray.toString())
+console.log(numarray.reduce(state));
 
-      console.log(newArray)
+fetch("https://jsonplaceholder.typicode.com/posts")
+.then((response) => {
+  //console.log(response.json())
+  
+ JSON.stringify(response.json())
 
-      console.log(newArray.find(element => element < 20))
+})
 
+//   axios.get("https://jsonplaceholder.typicode.com/posts")
+//   .then((response) => {
+//     console.log(response);
 
-    //   axios.get("https://jsonplaceholder.typicode.com/posts")
-    //   .then((response) => {
-    //     console.log(response);
+//     data.map((item) => {
 
-    
+//       `<tr>
+//                 <td></td>
+//                 <td></td>
+//                 <td></td>
 
-
-
-    //     data.map((item) => {
-
-    //       `<tr>
-    //                 <td></td>
-    //                 <td></td>
-    //                 <td></td>
-                    
-    //                 </tr>`;
-    //     });
-    //   });
+//                 </tr>`;
+//     });
+//   });
